d058f251ebfa5972fdc6c1aeea83bf003644d2a9
Bharath Vishal G <bharathvishalg@gmail.com>
2023-02-19 23:24:53 +0530

    Dev commit

--- app/src/main/java/com/bharathvishal/appmanager/Activities/MainActivity.kt
+++ app/src/main/java/com/bharathvishal/appmanager/Activities/MainActivity.kt
@@ -21,6 +21,23 @@ import com.google.android.material.color.DynamicColors
 import kotlinx.coroutines.*
 import java.lang.ref.WeakReference
 
+/**
+ *
+ * Copyright 2018-2023 Bharath Vishal G.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **/
 
 class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     private var adapter: AppsAdapter? = null
