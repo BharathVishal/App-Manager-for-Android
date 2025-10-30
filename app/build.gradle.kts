71b5f87d6ae1ddb2858c7111f1ab11c6d4873022
Bharath Vishal G <bharathvishalg@gmail.com>
2025-10-30 23:13:45 +0530

    dev commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         }
         minSdk = 23
         targetSdk = 36
-        versionCode = 105
-        versionName = "4.5"
+        versionCode = 110
+        versionName = "4.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
