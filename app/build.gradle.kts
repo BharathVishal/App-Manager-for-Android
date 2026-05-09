a1bdade1f29197b011eb49475fcd049c837b8a4a
Bharath Vishal G <bharathvishalg@gmail.com>
2026-05-09 20:37:37 +0530

    Dev commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,11 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import com.android.build.api.dsl.ApplicationExtension
 
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.compose.compiler)
 }
 
-android {
+extensions.configure<ApplicationExtension> {
     namespace = "com.bharathvishal.appmanager"
     buildFeatures.buildConfig = true
     compileSdk = 36
@@ -43,15 +43,12 @@ android {
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget = JvmTarget.JVM_17
-        }
-    }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.15"
     }
